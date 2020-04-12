48fffbc9395b9a9cf388c49b40f93b21e6deb50e
nadiaGincoff <nadia.gincoff@gmail.com>
2020-04-11 20:53:02 -0400

    agregando funcionalidad a los botones

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import validator from './validator.js';
-// import { parse } from "@babel/parser"
 
+// console.log(validator);
+
+///  VARIABLES 
+
+const pantallaDeBienvenida = document.getElementById("pantalla-dividida");
+const informacion = document.getElementById("informacion");
+const btnRegistrarse = document.getElementById("btnRegistrarse");
+const formulario = document.getElementById("formulario");
+const btnEnviar = document.getElementById("btnEnviar");
+const mensajeFinal = document.getElementById("mensajeFinal");
+let elementosDeTarjeta = document.getElementById("numeroDeTarjeta").textContent;
+
+// console.log(elementosDeTarjeta);
+
+///   FUNCIONES 
+
+// Mostrar el formulario y ocultar la primer pantalla de informacion y boton registro
 function mostrarFormulario() {
-    let registro = document.getElementById("registro");
-    let formulario = document.getElementById("formulario");
-   
-    registro.style.display = "none";
+    pantallaDeBienvenida.style.display = "none";
+    informacion.style.display = "none";
+    btnRegistrarse.style.display = "none";
     formulario.style.display = "block";
 }
 
-console.log(validator);
+// 
+function enviarFormulario() {
+    formulario.style.display = "none";
+    mensajeFinal.style.display = "block";
+    let elementosDeTarjeta = document.getElementById("numeroDeTarjeta").value;
+}
+
+///   EVENTOS 
+
+// Agrego funcionalidad al boton registrarse.
+btnRegistrarse.addEventListener("click", function(event){
+    mostrarFormulario();
+}) 
+
+// Agrego evento "submit" para enviar formulario 
+btnEnviar.addEventListener("submit", function(event){
+    enviarFormulario();
+    // validator.isValid();
+}) 
+
+
+
+
