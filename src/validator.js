e09ae06f3915268abe4c1cc7d8dd484e0703359e
nadiaGincoff <nadia.gincoff@gmail.com>
2020-04-11 20:54:22 -0400

    numero multiplicado y sumado

--- src/validator.js
+++ src/validator.js
@@ -1,15 +1,7 @@
-// TODO:
-// Preguntar por el numero de la tarjeta
-// Pasar los numeros a un array en orden inverso
-// Aplicar la operacion a los numeros de las posiciones parse
-// Sumar los digitos y nuevos digitos
-// Comprobar si es una tarjeta validTa
-
-// import  from "@babel/types";
-
-// let creditNumber = document.getElementById("targetNumber").value;
+// import { stringLiteral } from "@babel/types";
 
 let creditCardNumber = "4546420024453980";
+// let creditCardNumber1 = [4,5,4,6,4,2,0,0,2,4,4,5,3,9,8,0];
 
 // 1st for() = variable contador = la longitud del string menos 1,
 // ya que los array comienzan en 0
@@ -18,53 +10,67 @@ let creditCardNumber = "4546420024453980";
 
 const validator = {
 
- isValid: function(creditCardNumber) {
-    // pasar el string a array  
-    let arrCreditCardNumber = [];
-    // pasar de string a array 
-    for (let i = creditCardNumber.length -1; i >= 0; i--) {
-    // Devuelve el caracter de una posicion dada mediante char.At   
-        let caracter = creditCardNumber.charAt(i);
-    // Guardo cada caracter como un numero
-        let numero = parseInt(caracter);
-    // Agrego al array mediante push
-        arrCreditCardNumber.push(numero);
-    }
-    console.log(arrCreditCardNumber + "FUNCIONAAAA ESOOOOO");
-    
-
-
-    // Creo un nuevo array para los numeros multiplicados
-    let numberMultiplicate = [];
-    // Recorro el array con el forEach, poniendo el numero a multiplicar[elemento del array], y la posicion[array]
-    arrCreditCardNumber.forEach(function(number, posicion) {
-    // si la posicion es multiplo de 2, agrega el numero multiplicado por 2 al array.
-        if (posicion % 2 === 0) {
-            let numberMultiplicado = (number * 2);
-    // si el numero multiplicado es mayor a 10
-            if (numberMultiplicado >= 10) {
-                let posicion1 = numberMultiplicado.toString().charAt(0, 1);
-                let posicion2 = numberMultiplicado.toString().charAt(1, 2);
-                let numberSumado = parseInt(posicion1) + parseInt(posicion2);
-                console.log(posicion1);
-                console.log(posicion2);
-                console.log(numberSumado);
-                numberMultiplicate.push(numberSumado);
-            } 
-    // si no, agrega el numero sin multiplicar 
-        } else {
-            numberMultiplicate.push(number);
+    isValid: function (creditCardNumber) {
+        // pasar el string a array  
+        let arrCreditCardNumber = [];
+        // pasar de string a array 
+        for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+            // Devuelve el caracter de una posicion dada mediante char.At   
+            let caracter = creditCardNumber.charAt(i);
+            // Guardo cada caracter como un numero
+            let numero = parseInt(caracter);
+            // Agrego al array mediante push
+            arrCreditCardNumber.push(numero);
         }
-    })
-    console.log(numberMultiplicate + "VAMOO");
- 
- }
+
+        console.log(arrCreditCardNumber);
+        // Creo un nuevo array para guardar los numeros finales
+        let numeroMultiplicadoYSumado = [];
+        // Recorro el array con el forEach, poniendo el numero a multiplicar[elemento del array], y la posicion[array]
+        arrCreditCardNumber.forEach(function (numero, posicion) {
+            // si la posicion es multiplo de 2, agrega el numero multiplicado por 2 al array.
+            if (posicion % 2 === 0) {
+                let numeroMultiplicado = (numero * 2);
+                // si el numero multiplicado es mayor a 10
+                if (numeroMultiplicado >= 10) {
+                    let posicionPrimerDigito = numeroMultiplicado.toString().charAt(0, 1);
+                    let posicionSegundoDigito = numeroMultiplicado.toString().charAt(1, 2);
+                    let numeroSumado = parseInt(posicionPrimerDigito) + parseInt(posicionSegundoDigito);
+                    // console.log(posicion1);
+                    // console.log(posicion2);
+                    // console.log(numberSumado);
+                    numeroMultiplicadoYSumado.push(numeroSumado);
+                } else {
+                    numeroMultiplicadoYSumado.push(numeroMultiplicado);
+                }
+                // si no, agrega el numero sin multiplicar 
+            } else {
+                numeroMultiplicadoYSumado.push(numero);
+            }
+        })
+        console.log(numeroMultiplicadoYSumado);
+    }
+
+    
+        
 };
+ // todavia haciendo pruebas !
+
+ 
+      function sumarElementos(numeroFinal) {
+          console.log(numeroFinal);
+          let suma = 0;
+          for (let i = 0; i = numeroFinal.length; i++){
+              suma += numeroFinal[i];
+          }
+          console.log(sumarElementos(creditCardNumber1));
+          return suma;
+          
+      }
+      
 
 
 
-
-validator.isValid(creditCardNumber)
-
+// validator.isValid(creditCardNumber);
 
 export default validator;
