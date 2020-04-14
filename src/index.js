0413bb9a22ac2388cc6f46c579248ddbab895aa7
Nadia080290 <ngallardome@gmail.com>
2020-04-14 11:50:30 -0400

    Funcionalidad de botones

--- src/index.js
+++ src/index.js
@@ -4,42 +4,48 @@ import validator from './validator.js';
 
 ///  VARIABLES 
 
-const pantallaDeBienvenida = document.getElementById("pantalla-dividida");
-const informacion = document.getElementById("informacion");
-const btnRegistrarse = document.getElementById("btnRegistrarse");
+const pantallaDeBienvenida = document.getElementById("pantallaBienvenida");
 const formulario = document.getElementById("formulario");
+const btnRegistrarse = document.getElementById("btnRegistrarse");
+const btnValidar = document.getElementById("btnValidar");
 const btnEnviar = document.getElementById("btnEnviar");
 const mensajeFinal = document.getElementById("mensajeFinal");
-let elementosDeTarjeta = document.getElementById("numeroDeTarjeta").textContent;
-
-// console.log(elementosDeTarjeta);
 
 ///   FUNCIONES 
 
 // Mostrar el formulario y ocultar la primer pantalla de informacion y boton registro
 function mostrarFormulario() {
-    pantallaDeBienvenida.style.display = "none";
-    informacion.style.display = "none";
-    btnRegistrarse.style.display = "none";
+    pantallaBienvenida.style.display = "none";
     formulario.style.display = "block";
 }
 
-// 
 function enviarFormulario() {
     formulario.style.display = "none";
     mensajeFinal.style.display = "block";
-    let elementosDeTarjeta = document.getElementById("numeroDeTarjeta").value;
 }
 
 ///   EVENTOS 
-
 // Agrego funcionalidad al boton registrarse.
 btnRegistrarse.addEventListener("click", function(event){
     mostrarFormulario();
-}) 
+})
 
+btnValidar.addEventListener("click", function(event){
+    let numeroDeTarjeta = document.getElementById("creditCardNumber").value;
+    let tarjetaEnmascarada = validator.maskify(numeroDeTarjeta);
+    let validar = validator.isValid(numeroDeTarjeta);
+    if(validar){
+        document.getElementById("tarjetaValidada").innerHTML = "Tarjeta válida"
+        document.getElementById("creditCardNumber").value = tarjetaEnmascarada;
+        btnEnviar.disabled = false;
+        btnValidar.disabled = true;
+    } else {
+        document.getElementById("tarjetaValidada").innerHTML = "La tarjeta ingresada es incorrecta";
+    }
+})
 // Agrego evento "submit" para enviar formulario 
-btnEnviar.addEventListener("submit", function(event){
+formulario.addEventListener("submit", function(event){
+    // Cancela el evento del submit, previene la pagina de recargarse  
+    event.preventDefault();
     enviarFormulario();
-    // validator.isValid();
-}) 
+})
