4dfae1b099f253d1d797f653394d9a936289b21e
Nadia080290 <ngallardome@gmail.com>
2020-04-14 11:53:09 -0400

    Test funcionando

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -5,24 +5,25 @@ describe('validator', () => {
   test('debería ser un objeto', () => {
     expect(typeof validator).toBe('object');
   });
+});
 
   describe('validator.isValid', () => {
     test('debería ser una función', () => {
       expect(typeof validator.isValid).toBe('function');
     });
-
-    test.skip('debería retornar true para "4083952015263"', () => {
-      // escribe aquí tu test
+    test('debería retornar validator.isValid = true', () => {
+     let resultado = validator.isValid("4556364607935616");
+     expect(resultado).toBe(true);
     });
-  });
+    });
 
   describe('validator.maskify', () => {
     test('debería ser una función', () => {
       expect(typeof validator.maskify).toBe('function');
     });
 
-    test.skip('Debería retornar "############5616" para "4556364607935616"', () => {
-      // escribe aquí tu test
+    test('Debería retornar "############5616" para "4556364607935616"', () => {
+      let tarjeta =validator.maskify("4556364607935616");
+      expect(tarjeta).toBe("############5616")
     });
   });
-});
